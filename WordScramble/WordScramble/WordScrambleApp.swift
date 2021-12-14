c126cae8a9dcd16a75c669b4221dcb83e645ec8e
LuthforKhan <lrkhan@umich.edu>
2021-12-14 09:31:29 -0500

    made new project for the next challenge in the 100 days fo swift

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Luthfor Khan on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
