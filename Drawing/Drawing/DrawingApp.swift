12071be0f306044e210845d98be80d9e7d569efd
LuthforKhan <lrkhan@umich.edu>
2021-12-23 08:55:20 -0500

    new project Drawing

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Luthfor Khan on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
