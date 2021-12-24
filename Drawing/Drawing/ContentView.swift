2029ffbe2ffbbdb211c705e8949187514e96710f
LuthforKhan <lrkhan@umich.edu>
2021-12-24 13:27:54 -0500

    exploring the shape protocall

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,10 +7,38 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Square: Shape {
+    func path(in tri: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: tri.minX, y: tri.minY))
+        path.addLine(to: CGPoint(x: tri.minX, y: tri.maxY))
+        path.addLine(to: CGPoint(x: tri.maxX, y: tri.maxY))
+        path.addLine(to: CGPoint(x: tri.maxX, y: tri.minY))
+        path.addLine(to: CGPoint(x: tri.minX, y: tri.minY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Triangle()
+            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 200, height: 200)
     }
 }
 
